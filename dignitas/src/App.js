748da1c1028dedf99d6baf62a566dda2e40aeedf
60iTamas <80523010+60iTomi@users.noreply.github.com>
2025-07-08 09:23:58 +0000

    feat: initialize project with React setup, including CSS

---
+++ dignitas/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './fonts/NimbusSanL.css';
+import './fonts/MaterialIcons.css';
+import { EntryCard } from './components';
+
+function App() {
+  return (
+    <EntryCard />
+  );
+}
+
+export default App;
---
+++ dignitas/src/components/entryCard.js
@@ -0,0 +1,17 @@
+export default function EntryCard() {
+    return <div className="cardHolder">
+        <div className="cardBackground">
+            <div className="topLine"></div>
+            <div className="header">
+                <h1 className="establishmentName">KFC Győr, Baross út</h1>
+                <span className="establishmentSymbol">
+                    <span className="materialIcons">wc</span>
+                </span>
+            </div>
+            <hr className="ripHere"/>
+            <div className="bottomContent">
+                <h2 className="turnHere">{"<<<<<<<<"}</h2>
+            </div>
+    </div>;
+    </div>
+}
