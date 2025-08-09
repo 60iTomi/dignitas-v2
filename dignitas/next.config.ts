189299ac43c3d3ebe599c305da887e51e0cfb7f4
60iTamas <80523010+60iTomi@users.noreply.github.com>
2025-08-09 15:45:20 +0000

    migrated to next.js

---
+++ dignitas/next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+
+module.exports = {
+  i18n: {
+    locales: ['en', 'hu'],
+    defaultLocale: 'en'
+  },
+}
+export default nextConfig;
---
+++ dignitas/src/app/page.tsx
@@ -0,0 +1,10 @@
+import './globals.css';
+import './fonts/NimbusSanL.css';
+import './fonts/MaterialIcons.css';
+import './fonts/SpaceMono.css';
+
+export default function Home() {
+  return (
+    <div>wip</div>
+  );
+}
